36db8d9308147932e9b3adf8cfae9138104f1d2f
s-ferrer <saraferrer@gmail.com>
2022-07-07 16:35:50 +0200

    Create and style Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+import trollImage from "../images/troll.png";
 
 export default function Header() {
   return (
     <div className="header">
-      <img src={globeImage} alt="globe icon" className="globe" />
-      <p className="title">Travel Journal</p>
+      <div className="logoText">
+        <img src={trollImage} alt="troll face" className="troll" />
+        <p className="title">Meme Generator</p>
+      </div>
+      <p className="subtitle">React Course Project-3</p>
     </div>
   );
 }
