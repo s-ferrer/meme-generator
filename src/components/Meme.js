16e1914b1ffc81d200075ec18f5c72fa663fa41b
s-ferrer <saraferrer@gmail.com>
2022-07-07 18:10:31 +0200

    Add memesData file

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,15 @@
 import React from "react";
+/* import memesData from "../memesData.js"; */
 
 export default function Meme() {
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input className="form-input" type="text" placeholder="left-text" />
 
         <input className="form-input" type="text" placeholder="right-text" />
         <button className="form-button">Get a new meme image 🖼</button>
-      </form>
+      </div>
     </main>
   );
 }
