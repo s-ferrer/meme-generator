68f7ae6b226df1b7d8e72400af2fa943832c5f96
s-ferrer <saraferrer@gmail.com>
2022-07-08 12:56:54 +0200

    Use state to render meme images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,25 @@
 import React from "react";
-/* import memesData from "../memesData.js"; */
+import memesData from "../memesData.js";
 
 export default function Meme() {
+  const [memeImg, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
   return (
     <main>
       <div className="form">
         <input className="form-input" type="text" placeholder="left-text" />
 
         <input className="form-input" type="text" placeholder="right-text" />
-        <button className="form-button">Get a new meme image 🖼</button>
+        <button className="form-button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
       </div>
+      <img src={memeImg} className="meme-image" alt="meme" />
     </main>
   );
 }
